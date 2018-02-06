41d72459b0db0a7f37e1b9bec1d17be330490cbd
melaniebonnaudet <35600150+melaniebonnaudet@users.noreply.github.com>
2018-02-06 10:32:09 +0100

    fixed style tags

--- js/view/dishesView.js
+++ js/view/dishesView.js
@@ -1,6 +1,6 @@
 var AllDishesView = function (container, model) {    
     
-    $(document).ready(function() {
+   
         var key;
         var dishes;
         var allDishes = model.getDishes();
@@ -9,13 +9,11 @@ var AllDishesView = function (container, model) {
             dishes = '<div class="col-md-2" id="a'+ key +'"><div class="thumbnail"><img src="images/' + allDishes[key].image + '" style="width:100%"><div class="caption"><p id="dishName">' + allDishes[key].name + '</p></div></div></div>';       
             container.append(dishes);
             }
-    });  
-    
 }
 
 var StartersView = function (container, model) {    
     
-    $(document).ready(function() {
+   
         var key;
         var dishes;
         var allDishes = model.getDishes();
@@ -26,7 +24,5 @@ var StartersView = function (container, model) {
             if (allDishes[key].type == "starter") {
                  container.append(dishes);
             }  
-        }
-    });  
-    
+        }  
 }
